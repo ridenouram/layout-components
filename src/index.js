d77ea7a9337bb6d0013ce4a334563f5f81a65709
Anna Ridenour <annamridenour@gmail.com>
2019-05-13 12:03:05 -0700

    created basic list with css

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-console.log('hi');
-console.log('hhh');
+import styles from './index.css';
+
+const root = document.getElementById('root');
+
+const dogs = document.createElement('ul');
+dogs.className = styles.dogs;
+
+['spot', 'rover', 'bingo', 'joe'].forEach(name => {
+  const li = document.createElement('li');
+  li.textContent = name;
+  dogs.appendChild(li);
+});
+
+root.appendChild(dogs);
